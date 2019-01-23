0ca0d9a37f6b1a39ae2ff218da321a457ba5f6f7
unknown <sen.hakobyan@gmail.com>
2019-01-23 05:05:30 +0400

    protected fields defined in usub token model

--- src/Models/UsubToken.php
+++ src/Models/UsubToken.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsubToken extends Model
 {
+    protected $table = 'usub_tokens';
 
+    protected $fillable = [
+        'user1',
+        'user2',
+        'token',
+        'redirect_to',
+        'expires_at'
+    ];
 }
