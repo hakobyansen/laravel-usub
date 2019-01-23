82cca03aba4cea4b461d5efd0272e6e211d68b54
unknown <sen.hakobyan@gmail.com>
2019-01-23 05:46:58 +0400

    Routes fix

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::post('usub/sign-in', '\Usub\Http\Controllers\UsubTokensController@signin')->name('usub.sign_in');
-Route::post('usub/sign-out', '\Usub\Http\Controllers\UsubTokensController@signin')->name('usub.sign_out');
+Route::post('usub/sign-in', '\Usub\Http\Controllers\UsubTokensController@signIn')->name('usub.sign_in');
+Route::post('usub/sign-out', '\Usub\Http\Controllers\UsubTokensController@signOut')->name('usub.sign_out');
