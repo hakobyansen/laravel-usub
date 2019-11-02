c920bbf966768d0c32ca05320f8eddbf59905f21
c0d3b0t <sen.hakobyan@gmail.com>
2019-11-02 04:01:04 +0400

    null coalescing operator

--- src/Views/partials/sign_in.blade.php
+++ src/Views/partials/sign_in.blade.php
@@ -1,6 +1,6 @@
 <form data-user-id="{{ $user_id }}" action="{{ route('usub.sign_in') }}" method="post">
 	@csrf
 	<input type="hidden" name="user2" value="{{ $user_id }}">
-	<input type="hidden" name="redirect_to_on_sign_in" value="{{ $on_sign_in }}">
-	<input type="hidden" name="redirect_to_on_sign_out" value="{{ $on_sign_out }}">
+	<input type="hidden" name="redirect_to_on_sign_in" value="{{ $on_sign_in ?? '' }}">
+	<input type="hidden" name="redirect_to_on_sign_out" value="{{ $on_sign_out ?? '' }}">
 </form>
